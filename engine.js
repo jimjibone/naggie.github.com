fb5ea9b8f26ee28d8b80c60f99f4f7ce492941e3
Callan Bryant <callan.bryant@gmail.com>
2012-11-06 08:37:36 +0000

    remove ;

--- engine.js
+++ engine.js
@@ -1,50 +1,50 @@
 // Copyright Callan Bryant 2011-2012 <callan.bryant@gmail.com> http://callanbryant.co.uk
 // All rights reserved.
 
-//var converter = new Showdown.converter();
+//var converter = new Showdown.converter()
 $(function(){
 	// apply loading gif to each external external article prior to load.
-	$('article[data-src]').html('<div class="throbber"></div>');
+	$('article[data-src]').html('<div class="throbber"></div>')
 
-	generateNav();
+	generateNav()
 
 	$('#backdrop > img').hide().bind("load",function(){
-		$(this).fadeIn(3500);
+		$(this).fadeIn(3500)
 		// now is a good time to preload any articles
-		preloadArticles();
-	});
+		preloadArticles()
+	})
 
 	$('article .logos img').css('opacity',0).load(function(){
-		$(this).fadeTo('fast',0.7);
-	});
+		$(this).fadeTo('fast',0.7)
+	})
 
-	$('#contact').hide();
+	$('#contact').hide()
 
 	var hideQuote = function(){
-		$('#logo .quote').stop().fadeOut();
-		$('#logo h1').stop().animate({'margin-top':'50px'});
-	};
+		$('#logo .quote').stop().fadeOut()
+		$('#logo h1').stop().animate({'margin-top':'50px'})
+	}
 
 	setTimeout(function(){
-		hideQuote();
-		$('#logo').bind('mouseleave',hideQuote);
-	},4000);
+		hideQuote()
+		$('#logo').bind('mouseleave',hideQuote)
+	},4000)
 
 	$('#logo').bind('mouseenter',function(){
-		$('#logo .quote').stop().fadeIn();
-		$('#logo h1').stop().animate({'margin-top':'26px'});
-	});
+		$('#logo .quote').stop().fadeIn()
+		$('#logo h1').stop().animate({'margin-top':'26px'})
+	})
 
 	
 
 	var contactTimeout = setTimeout(function(){
-		$('#contact').hide().fadeIn();
-	},6000);
+		$('#contact').hide().fadeIn()
+	},6000)
 
 	$(window).one('scroll',function(){
-		clearTimeout(contactTimeout);
-		$('#contact').fadeIn();
-	});
+		clearTimeout(contactTimeout)
+		$('#contact').fadeIn()
+	})
 
 
 	// IE9 hack
@@ -52,122 +52,122 @@ $(function(){
 	// so trigger it manually...
 	if($.browser.msie && parseInt($.browser.version) >= 9 )
 		setTimeout(function(){
-			$('img').trigger('load');
-		},3000);
+			$('img').trigger('load')
+		},3000)
 
 	// left/right select service
 	$(document).bind('keydown',"right",function(){
 		// check to see if a service has been active
 		if (!$('.service').hasClass('active'))
-			var next = $('.service').first();
+			var next = $('.service').first()
 		else
-			var next = $('.active').next();
+			var next = $('.active').next()
 
-		next.each(showThisArticle);
-	});
+		next.each(showThisArticle)
+	})
 	$(document).bind('keydown',"left",function(){
 	// check to see if a service has been active
 		if (!$('.service').hasClass('active'))
-			var next = $('.service').first();
+			var next = $('.service').first()
 		else
-			var next = $('.active').prev();
+			var next = $('.active').prev()
 
-		next.each(showThisArticle);
-	});
+		next.each(showThisArticle)
+	})
 
 	// enter to go to the URL of the active service
 	$(document).bind('keyup','return',function(){
 		$('nav a.active').each(function(){
 			// restore postion
-			$('.active').css('top',0);
+			$('.active').css('top',0)
 			// do something else using active nav link or attachedarticle
-		});
+		})
 	}).bind('keydown','return',function(){
-		$('.active').css('position','relative').css('top','2px');
-	});
+		$('.active').css('position','relative').css('top','2px')
+	})
 	
-});
+})
 
 // create elements representing pages
 // with attached  events
 function generateNav()
 {
-	$('nav').empty();
+	$('nav').empty()
 	$('article[data-name]').each(function(index,art){
 		// jquery-ify
-		art = $(art);
+		art = $(art)
 
 		// safe URL hash for link
-		var hash = '#'+art.data('name').replace(/[^0-9a-z]+/gi,'-');
+		var hash = '#'+art.data('name').replace(/[^0-9a-z]+/gi,'-')
 
-		var link= $("<a />").addClass('service');
-		link.text(art.data('name'));
-		link.appendTo('nav');
-		link.click(showThisArticle);
+		var link= $("<a />").addClass('service')
+		link.text(art.data('name'))
+		link.appendTo('nav')
+		link.click(showThisArticle)
 
 		link.one('mouseenter',function(){
-			initArticle(art);
-		});
+			initArticle(art)
+		})
 
 		// attach reference to element so it can be shown later
-		link.data('article',art);
+		link.data('article',art)
 
 		// attach link
-		link.attr('href',hash);
+		link.attr('href',hash)
 
 		// title (to be made nice with bootstrap tooltips or tipsy)
-		link.attr('title', art.data('hint') );
+		link.attr('title', art.data('hint') )
 
 		if(document.location.hash == hash)
-			link.each(showThisArticle);
+			link.each(showThisArticle)
 
 		// no hash or empty hash
 		if (art.hasClass('default') && document.location.hash.length <=1)
-			link.each(showThisArticle);
-	});
+			link.each(showThisArticle)
+	})
 }
 
 // show article, given nav link in 'this' context
 function showThisArticle()
 {
-	var art = $(this).data('article');
+	var art = $(this).data('article')
 
 	// hide all other articles
-	$('article').hide();
+	$('article').hide()
 	// show this one
-	art.show().text();
+	art.show().text()
 
 	// clear active on all other service
-	$('nav .service').removeClass('active');
+	$('nav .service').removeClass('active')
 	// select this link 
-	$(this).addClass('active');
+	$(this).addClass('active')
 
 	// update hash location (when active, not clicked)
 	// also only give the default article a hash if explicitly active
 	if( !art.hasClass('default') || document.location.hash)
-		document.location.hash = $(this).attr('href');
+		document.location.hash = $(this).attr('href')
 
-	$('.articles header').html(  art.data('hint') );
+	$('.articles header').html(  art.data('hint') )
 
 	// need to init?
 	//if ( !$(this).data('article').data('ready') )
-		initArticle(art);
+		initArticle(art)
 }
 
 function preloadArticles(){
 	$('article[data-preload]').each(function(){
-		initArticle($(this));
-	});
+		initArticle($(this))
+	})
 }
 
 // initialise the article, given the mathing jQuery object
 function initArticle(art){
 	// only want to call this once...
 	if (art.data('ready'))
-		return false;
+		return false
 
 	// set flag so function is not called again
-	art.data('ready',true);
+	art.data('ready',true)
 	
 	// external HTML fragment, markdown
 	if ( art.data('src') ){
@@ -175,67 +175,67 @@ function initArticle(art){
 			$.getFeed({
 				url: art.data('src'),
 				error:function(){
-					art.text('Error retreiving feed');
+					art.text('Error retreiving feed')
 				},
 				success:function(feed){
-					var html = feed2html(feed);
-					art.html(html);
+					var html = feed2html(feed)
+					art.html(html)
 				}
 			})
 		else
 			$.ajax({
 				url: art.data('src'),
 				error:function(){
-					art.text('Error retrieving article');
+					art.text('Error retrieving article')
 				},
 				dataType: 'html',
 				success: function(html){
 					if (art.data('type') == 'markdown')
-						//html = converter.makeHtml(html);
-						html = 'Markdown disabled';
+						//html = converter.makeHtml(html)
+						html = 'Markdown disabled'
 			
-					art.html(html);
+					art.html(html)
 					// syntax highlighting
-					$('pre code',art).each(function(i, e) {hljs.highlightBlock(e)});
+					$('pre code',art).each(function(i, e) {hljs.highlightBlock(e)})
 
 				}
-			});
+			})
 	}	
 	// inline markdown
 	else if(art.data('type') == 'text/x-web-markdown'){
-		var html = $(art).html();
-		html = converter.makeHtml(html);
-		$(art).html(html);
+		var html = $(art).html()
+		html = converter.makeHtml(html)
+		$(art).html(html)
 
 	}
 
 	// syntax highlighting of inline articles
-	$('pre code',art).each(function(i, e) {hljs.highlightBlock(e)});
+	$('pre code',art).each(function(i, e) {hljs.highlightBlock(e)})
 }
 
 
 // given a string rss it must return some html... 
 function feed2html(feed){
-	var html = $('<div class="rss"></div>');
+	var html = $('<div class="rss"></div>')
 
 	for (var i in feed.items){
-		var post = $('<div class="container_12 post"><div class="grid_10 prefix_1 suffix_1 meta"></div><div class="content grid_10 prefix_1 suffix_1"></div></div>');
-		$('.meta',post).append('<h1><a href="'+feed.items[i].link+'">'+feed.items[i].title+'</a></h1>');
+		var post = $('<div class="container_12 post"><div class="grid_10 prefix_1 suffix_1 meta"></div><div class="content grid_10 prefix_1 suffix_1"></div></div>')
+		$('.meta',post).append('<h1><a href="'+feed.items[i].link+'">'+feed.items[i].title+'</a></h1>')
 
-		var date = new Date(feed.items[i].updated);
-		date = relativeDate(date);
+		var date = new Date(feed.items[i].updated)
+		date = relativeDate(date)
 
-		$('.meta h1',post).append('<span class="date">'+date+'</span>');
+		$('.meta h1',post).append('<span class="date">'+date+'</span>')
 
-		$('.meta',post).append('<div class="ref">'+feed.items[i].link.replace(/http:\/\//,'').match(/[a-z0-9\.]+/i)+'</div>');
+		$('.meta',post).append('<div class="ref">'+feed.items[i].link.replace(/http:\/\//,'').match(/[a-z0-9\.]+/i)+'</div>')
 
-		$('.content',post).append(feed.items[i].description);
+		$('.content',post).append(feed.items[i].description)
 	
 
-		html.append(post).append('<hr />');
+		html.append(post).append('<hr />')
 	}
 
-	return html;
+	return html
 }
 
 // Copyright Callan Bryant 2011-2012 <callan.bryant@gmail.com> http://callanbryant.co.uk
@@ -286,9 +286,9 @@ var relativeDate = (function(undefined){
 
   return relativeDate;
 
-})();
+})()
 
 if(typeof module != 'undefined' && module.exports){
-  module.exports = relativeDate;
+  module.exports = relativeDate
 }
 
