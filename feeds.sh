1dd2b71076a3c1c826ddd20ff1d0866ea196b23a
Callan Bryant <callan.bryant@gmail.com>
2012-11-11 19:43:32 +0000

    disable old blog feed wget:wq

--- feeds.sh
+++ feeds.sh
@@ -3,4 +3,4 @@
 cd $(dirname $0)
 
 curl 'http://www.google.com/reader/public/atom/user%2F15749961360086107608%2Fstate%2Fcom.google%2Fstarred' > starred.atom
-curl 'http://callanbryant.wordpress.com/feed/atom/' > blog.atom
+#curl 'http://callanbryant.wordpress.com/feed/atom/' > blog.atom
